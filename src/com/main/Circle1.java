6df1f84f582cd86932a1acfcb6d0eae3931be1cf
Deiv-Dev <nomaxas@gmail.com>
2022-09-26 18:25:53 +0100

    first commit

---
+++ src/com/main/Circle1.java
@@ -0,0 +1,21 @@
+package com.main;
+
+class Circle1 {
+
+    //Data fields
+    double radius;
+
+    //Constructors
+    Circle1() {
+        radius = 1.0;
+    }
+
+    //Methods
+    Circle1(double newRadius) {
+        this.radius = newRadius;
+    }
+
+    double getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
